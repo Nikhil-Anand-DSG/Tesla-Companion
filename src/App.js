99b3347529e8a1bff27218ca40e59701a26bb972
Nik Anand <nikhil.anand@dcsg.com>
2023-08-15 11:19:41 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,17 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Tesla Companion
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a href="https://youtube.com">
+          <button className="EnterButton" >
+            Youtube
+          </button>
+        </a>
+        <a href="https://reddit.com">
+          <button className="EnterButton" >
+            Reddit
+          </button>
         </a>
       </header>
     </div>
