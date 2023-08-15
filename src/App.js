9884b80d9b37b0502faf6ae4118eb6b55d069adc
Nik Anand <nikhil.anand@dcsg.com>
2023-08-15 11:55:10 -0400

    basics

--- src/App.js
+++ src/App.js
@@ -3,24 +3,27 @@ import './App.css';
 
 function App() {
   return (
+      <div>
+        <p className="text">Nikhil Anand</p>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" color="red" />
         <p>
           Tesla Companion
         </p>
-        <a href="https://youtube.com">
+        <a href="https://youtube.com/redirect?q=https://www.coingecko.com/en/coins/dogecoin">
           <button className="EnterButton" >
-            Youtube
+            Doge Coin
           </button>
         </a>
-        <a href="https://reddit.com">
+        <a href="https://youtube.com/redirect?q=https://reddit.com">
           <button className="EnterButton" >
             Reddit
           </button>
         </a>
       </header>
     </div>
+      </div>
   );
 }
 
