564986881f6dd5ea72220d2ce6df980d5f2e6580
Nik Anand <nikhil.anand@dcsg.com>
2023-08-16 11:17:46 -0400

    added more links, in flex box

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import reddit from "./Assets/reddit.png";
+import maps from "./Assets/maps.png";
+import cat from "./Assets/cat.png";
+import charge from "./Assets/charge.png";
+import "./App.css";
 
 function App() {
   return (
       <div>
         <p className="text">Nikhil Anand</p>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" color="red" />
-        <p>
-          Tesla Companion
-        </p>
-        <a href="https://youtube.com/redirect?q=https://www.coingecko.com/en/coins/dogecoin">
-          <button className="EnterButton" >
-            Doge Coin
-          </button>
-        </a>
-        <a href="https://youtube.com/redirect?q=https://reddit.com">
-          <button className="EnterButton" >
-            Reddit
-          </button>
-        </a>
-      </header>
-    </div>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" color="red" />
+            <p>Tesla Companion</p>
+            <div className="links">
+              <a href="https://youtube.com/redirect?q=https://www.reddit.com/r/teslamotors/">
+                <img src={reddit} className="link-button" alt="reddit" />
+              </a>
+              <a href="https://youtube.com/redirect?q=https://www.google.com/maps">
+                <img
+                    src={maps}
+                    className="link-button"
+                    alt="maps"
+                    style={{ height: 100, width: 120 }}
+                />
+              </a>
+                <a href="https://youtube.com/redirect?q=https://hostrider.com/">
+                    <img
+                        src={cat}
+                        className="link-button"
+                        alt="cat"
+                    />
+                </a>
+                <a href="https://youtube.com/redirect?q=https://www.plugshare.com/">
+                    <img
+                        src={charge}
+                        className="link-button"
+                        alt="charge"
+                    />
+                </a>
+            </div>
+          </header>
+        </div>
       </div>
   );
 }
